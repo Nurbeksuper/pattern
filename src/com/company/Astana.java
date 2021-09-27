afe5dec7f5b40eeb700c2b8f679daf22163d8ee9
nurbekgeometry <nurbekgeometry@gmail.com>
2021-09-27 18:23:50 +0600

    dsf

---
+++ src/com/company/Astana.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Astana implements Qwerty{
+    @Override
+    public void go() {
+
+    }
+    public void jeu() {
+
+    }
+}
---
+++ src/com/company/creational/abstractFactory/website/ManualTester.java
@@ -0,0 +1,11 @@
+package com.company.creational.abstractFactory.website;
+
+import com.company.creational.abstractFactory.Tester;
+
+public class ManualTester implements Tester {
+    @Override
+    public void testCode() {
+        System.out.println("manual getTester");
+        System.out.println("manual getTester");
+    }
+}

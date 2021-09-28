3a0c4f9c3390768123c0288ac909e355df85f45a
nurbekgeometry <nurbekgeometry@gmail.com>
2021-09-28 17:56:29 +0600

    Builder

---
+++ src/com/company/creational/singleton/ProgamRunner.java
@@ -0,0 +1,11 @@
+package com.company.creational.singleton;
+
+public class ProgamRunner {
+    public static void main(String[] args) {
+        ProgramLogger.getProgramLogger().addLogInfo("First log...");
+        ProgramLogger.getProgramLogger().addLogInfo("Second log...");
+        ProgramLogger.getProgramLogger().addLogInfo("Third log...");
+
+        ProgramLogger.getProgramLogger().showLogFile();
+    }
+}
